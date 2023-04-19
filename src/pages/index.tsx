2e5194c446034a071646a55702a569ec80df491a
ITninja04 <ojaybock@gmail.com>
2023-04-19 14:28:58 -0500

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,123 +1,78 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import { useLayoutEffect } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export default function Home() {
+  useLayoutEffect(() => {
+    document.querySelectorAll('.grid-background > div').forEach((elem) => {
+      // Calculate random number for delay
+      var delay = getRandomInt(0, 5);
+      // Calculate random number for duration
+      var duration = getRandomInt(3, 6);
+
+      // Set both
+      elem.style.animationDelay = `${delay}s`;
+      elem.style.animationDuration = `${duration}s`;
+
+    })
+  }, [])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="relative min-h-screen flex items-center justify-center bg-gray-900 overflow-hidden">
+      <div className="grid-background absolute inset-0 p-2 grid grid-cols-12 gap-2 transform -skew-y-12 scale-150">
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-7 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+
+        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+
+        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-7 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+
+        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+        <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="relative">
+        <h2 className="text-white text-4xl md:text-8xl font-bold flex flex-row items-center">
+          Coming
+          <div className="relative text-sm mx-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill text-blue-500" viewBox="0 0 16 16">
+              <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+            </svg>
+            <div className="absolute -top-12 transform -rotate-45 text-blue-500"><p className="font-light text-base text-white bg-blue-500 rounded-md px-2 py-0">super</p></div>
+          </div>
+          Soon
+        </h2>
       </div>
     </main>
   )
