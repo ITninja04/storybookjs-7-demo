31f2a6f12923efbe0e971f1642d0ad442e64dc23
ITninja04 <ojaybock@gmail.com>
2023-04-19 14:06:21 -0500

    pivoted to Next JS 13

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import bluemodus_theme from './theme';
+
+addons.setConfig({
+  theme: bluemodus_theme,
+});
---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import {create} from '@storybook/theming/create';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'BlueModus',
+    brandUrl: 'https://bluemodus.com',
+    brandImage: 'https://bluemodus.com/img/content-images/logowhite.svg',
+})
